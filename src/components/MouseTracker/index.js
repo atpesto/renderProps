66a4992e1fbe28ea30c57e3e8bb1746e5ab939e9
Nikhil Arora <narora200@gmail.com>
2018-06-22 19:45:48 +0530

    Basic MouseTracker setup

--- src/components/MouseTracker/index.js
+++ src/components/MouseTracker/index.js
@@ -1,5 +1,27 @@
 import React, { Component } from 'react';
 
 class MouseTracker extends Component {
+  state = {
+    xPos: 0,
+    yPos: 0,
+  }
 
+  onMouseMoveHandler = (e) => {
+    this.setState({
+      xPos: e.clientX,
+      yPos: e.clientY,
+    })
+  }
+
+  render() {
+    return (
+      <div onMouseMove={this.onMouseMoveHandler}>
+        <p>X Position is: {this.state.xPos}</p>
+        <p>Y Position is: {this.state.yPos}</p>
+      </div>
+    );
+  }
 }
+
+
+export default MouseTracker;
